2e97e2c2267618b05124f062b5b820d3a78db695
Chirag Rohada <115901486+ChiragRohada7020@users.noreply.github.com>
2025-02-06 02:40:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -267,5 +267,6 @@ def delete_transaction(transaction_id):
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
